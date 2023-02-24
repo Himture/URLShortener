b0fb544ba75d4227fe11138899545e6575540700
Himanshu Agarwal <everythinghimanshu@gmail.com>
2023-02-24 15:45:24 +0530

    Endpoints dont have https

--- frontend/src/logic/gql.ts
+++ frontend/src/logic/gql.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient, gql } from 'graphql-request'
 import { remToken, setToken } from './auth'
 
-const endpoint = 'http://graphql-production.himanshuoslash.workers.dev'
+const endpoint = 'graphql-production.himanshuoslash.workers.dev'
 const token = localStorage.getItem('Oslash')
 const graphQLClient = new GraphQLClient(endpoint, {
   headers: {
