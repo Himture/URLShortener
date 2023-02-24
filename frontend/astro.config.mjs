bb1330c1e24a2b8a09d3a78567e79ec745f7ea6a
Himanshu Agarwal <everythinghimanshu@gmail.com>
2023-02-24 14:33:57 +0530

    Fixing Cloudflare pages

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -10,3 +10,9 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()]
 });
+
+module.exports = {
+  moduleNameMapper: {
+    "#node-web-compat": "./node-web-compat-node.js",
+  },
+};
