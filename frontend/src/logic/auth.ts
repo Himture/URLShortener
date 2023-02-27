964bfba3167d597faed127e91e4e5bddd78f34c0
Himanshu Agarwal <everythinghimanshu@gmail.com>
2023-02-27 12:23:55 +0530

    Added local storage to if clause

--- frontend/src/logic/auth.ts
+++ frontend/src/logic/auth.ts
@@ -1,18 +1,24 @@
 
 export function setToken(token:string) {
-    localStorage.setItem('Oslash', token)
+    if (typeof window !== 'undefined') {
+        localStorage.setItem('Oslash', token)
+    }
 }
 
 export function remToken(){
-    localStorage.removeItem('Oslash')
+    if (typeof window !== 'undefined') {
+        localStorage.removeItem('Oslash')
+    }
 }
 
 export async function authenticate() {
-    const token = localStorage.getItem('Oslash')
+    if (typeof window !== 'undefined') {
+        const token = localStorage.getItem('Oslash')
     if(token){
         return "Pass"
     }
     else{
         return null
+    }
     }
 }
