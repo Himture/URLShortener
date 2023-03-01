45d9ced261ed645b64a9af8bafe8f5d932c3d26d
Himanshu Agarwal <everythinghimanshu@gmail.com>
2023-03-01 19:20:53 +0530

    Improved UI

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,5 @@
 import { authenticate } from "../logic/auth";
 import { useState, useEffect } from "react";
-import { logout } from "../logic/gql";
 
 export default function Header() {
   const [token, setoken] = useState();
@@ -15,6 +14,8 @@ export default function Header() {
   }, []);
 
   return (
+    <>
+    {token == null ? (
     <header>
       <nav className=" border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
         <div className="container flex flex-wrap items-center justify-between mx-auto">
@@ -47,8 +48,6 @@ export default function Header() {
           </button>
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              {token == null ? (
-                <>
                   <li>
                     <a
                       href="/login"
@@ -73,45 +72,12 @@ export default function Header() {
                       Confirm Code
                     </a>
                   </li>
-                </>
-              ) : (
-                <>
-                  <li>
-                    <a
-                      href="/"
-                      className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                    >
-                      Home
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/createlink"
-                      className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                    >
-                      Create Link
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                    >
-                      <button onClick={lOut}>Logout</button>
-                    </a>
-                  </li>
-                </>
-              )}
             </ul>
           </div>
         </div>
       </nav>
-    </header>
-  );
-}
-
-async function lOut() {
-  const res = await logout();
-  window.alert(res);
-  window.location = "/";
+    </header> ): (
+                <>
+                </>)}
+    </>)
 }
--- frontend/src/components/ShowAllLinks.jsx
+++ frontend/src/components/ShowAllLinks.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { authenticate } from "../logic/auth";
 import {
   allUserURL,
@@ -6,6 +6,7 @@ import {
   incrementalSearch,
   updateUrl,
   addTag,
+  logout
 } from "../logic/gql";
 import "./App.css";
 import NotLogedIn from "./NotLogedIn";
@@ -16,8 +17,10 @@ export default function ShowAllLinks() {
   const [res, setRes] = useState();
   const [tags, settags] = useState();
   const [search, setSearch] = useState("");
-  const [activeTab, setActiveTab] = useState("hidden");
+  const [activeTab, setActiveTab] = useState("All");
   const [isLoading, setIsLoading] = useState(false);
+  const [showAll, setShowAll] = useState()
+  const count = useRef(0);
 
   useEffect(() => {
     async function getToken() {
@@ -27,16 +30,19 @@ export default function ShowAllLinks() {
     }
     getToken();
     if (!search) {
-      async function getData() {
+      setIsLoading(true)
+      let debounce = setTimeout(async () => {
         await allUserURL(token).then((t) => {
           setRes(t.links);
           const rectag = [...new Set(t.links?.map((links) => links.tag).flat()),];
           rectag.sort();
+          console.log("calling api")
           settags(rectag);
+          setShowAll(true)
+          count.current = 0
           setIsLoading(false)
-        });
-      }
-      getData();
+      })}, 300)
+      return () => clearTimeout(debounce);
     } else {    
       setIsLoading(true)
       let debounce = setTimeout(async () => {
@@ -45,6 +51,7 @@ export default function ShowAllLinks() {
         console.log(data);
         setRes(data);
         setIsLoading(false)
+        count.current = 0
       }, 500);
       return () => clearTimeout(debounce);
     }
@@ -56,29 +63,37 @@ export default function ShowAllLinks() {
     console.log("You clicked search");
   }
 
+  async function handleLogout(event) {
+    const res = await logout()
+    window.alert(res)
+  }
+
   async function handleDelete(event) {
-    if (confirm("Are you sure to delete o/" + event.target.value)) {
-      const res = await deleteUrl(event.target.value);
+    if (confirm("Are you sure to delete o/" + event.target.id)) {
+      const res = await deleteUrl(event.target.id);
       window.alert(res);
       window.location.reload();
     }
   }
 
   async function handleTag(event) {
-    const sLink = event.target.value;
+    const sLink = event.target.id;
     const val = prompt("Enter the tag for o/" + sLink);
     if (val) {
       const res = await addTag(sLink, val);
       window.alert(res);
+      window.location.reload()
     }
   }
 
   async function handleEdit(event) {
-    const sLink = event.target.value;
+    const sLink = event.target.id;
+    console.log(event.target)
     const val = prompt("Enter new link for o/" + sLink);
     if (val) {
       const res = await updateUrl(sLink, val);
       window.alert(res);
+      window.location.reload();
     }
   }
 
@@ -87,53 +102,54 @@ export default function ShowAllLinks() {
     setActiveTab(tab);
   }
 
-  const linkBox = (links) => (
-    <div
-      key={links.sLink}
-      className={`tabcontent item bg-white m-10 rounded-lg`}
-    >
-      <div className="item-info flex-none">
-        <p id={links.oLink} className="text-lg">
+  const linkBox = (links) => {
+  return(
+    <div key={links.sLink} className={`tabcontent item bg-white m-5 rounded-lg group w-2/3`}>
+      <img height="50" width="50" src={`http://favicon.yandex.net/favicon/${links.oLink}`} />
+      <div className="item-info flex-none ml-5">
+        <p id={links.oLink} className="text-xl">
           <a href={"https://" + links.oLink} target="_blank">
             o/{links.sLink}
           </a>
         </p>
-        <p id={links.sLink} className="text-sm">
+        <p id={links.sLink} className="text-xs">
           {links.oLink}
         </p>
-        <p id={links.tag}>{links.tag}</p>
+        <p id={links.tag} className="text-sm text-slate-500	">
+          {links.tag}
+        </p>
       </div>
-      <div className="item-actions">
-        <button
-          value={links.sLink}
-          onClick={handleEdit}
-          className="edit-btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Edit
+      <div className="item-actions invisible group-hover:visible">
+        <button onClick={handleEdit} className="m-2">
+          <svg id={links.sLink} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 overflow:visible" viewBox="0 0 20 20" fill="currentColor">
+            <path id={links.sLink} d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+          </svg>
         </button>
-        <button
-          value={links.sLink}
-          onClick={handleTag}
-          className="edit-btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Edit Tag
+        <button onClick={handleTag} className="m-2">
+          <svg id={links.sLink} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+            <path id={links.sLink} fillRule="evenodd" d="M17.707 9.293a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-7-7A.997.997 0 012 10V5a3 3 0 013-3h5c.256 0 .512.098.707.293l7 7zM5 6a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
+          </svg>
         </button>
-        <button
-          value={links.sLink}
-          onClick={handleDelete}
-          className="delete-btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Delete
+        <button onClick={handleDelete} className="m-2">
+          <svg id={links.sLink}  xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+            <path id={links.sLink} fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+          </svg>
         </button>
       </div>
-    </div>)
+    </div>)}
 
   const boxCheck = (
     <div>
       {res?.map((links) => {
-        if (activeTab == links.tag && !search) {
+        if(activeTab == "All"){
+          count.current = count.current + 1
+          return(linkBox(links))
+        }
+        else if (activeTab == links.tag && !search) {
+          count.current = count.current + 1
           return linkBox(links);
         } else if (search) {
+          count.current = count.current + 1
           return linkBox(links);
         }
       })}
@@ -142,74 +158,73 @@ export default function ShowAllLinks() {
 
   const links = (
     <div>
-      <div className="flex">
+      <div className="flex border-b-2 pb-0">
+      {!search ? <div style={activeTab == "All" ? {borderBottom: "3px solid blue", borderRadius: "-4px", color: "black"} : {color: "grey"}} className="mt-5 ml-5 mb-0 px-2 flex pb-5 justify-center"><button name="All" onClick={handleTab} className={"text-md pt-0"}>All</button></div> : <></>}
         {tags?.map((tag) => {
           return (
             <div key={tag}>
               {!search ? (
-                <button
-                  name={tag}
-                  onClick={handleTab}
-                  className={"mt-10 ml-10 mb-0 " + (activeTab == tags ? "text-s" : "text-2xl")}
-                >
-                  {tag}
-                </button>
-              ) : (
-                <></>
-              )}
-            </div>
-          );
+              <div style={activeTab == tag ? {borderBottom: "3px solid blue", borderRadius: "-4px", color: "black"} : {color: "grey"}} className="mt-5 ml-5 mb-0 px-2 flex pb-5 justify-center">
+                <button name={tag} onClick={handleTab} className={"text-md pt-0"}> {tag} </button>
+              </div>
+              ) : (<></>)}
+            </div>);
         })}
       </div>
-      {boxCheck}
+      <div className="pl-5 pt-5">
+        <h1>Showing {count.current} results.</h1>
+      </div>
+      <div className="mt-5">
+      {boxCheck}</div>
     </div>
   );
 
   if (token) {
     return (
       <>
-        <div className=" bg-slate-500 p-2">
-          <label
-            htmlFor="default-search"
-            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-          >
+      <div className="flex justify-center items-center p-5 border-b-2">
+        <div className="w-1/2 mr-auto">
+          <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
             Search
           </label>
-          <div className="mx-10 mt-10 relative">
+          <div className="relative">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                ></path>
+              <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" >
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
               </svg>
             </div>
-            <input
-              onChange={handleSearch}
-              type="search"
-              id="default-search"
-              className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search for shortlinks"
-              required
+            <input onChange={handleSearch} type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border-2 border-blue-700 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required
             />
-            <button
-              type="submit"
-              className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Search
-            </button>
           </div>
-          <div className="min-h-screen stock-container">{isLoading ? <LoadingSpinner /> : links}</div>
         </div>
+        <div className="ml-auto flex items-center justify-center space-x-4">
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
+            </svg>
+          </button>
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
+            </svg>
+          </button>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      </div>
+      <div className="px-5 pt-5 pb-0 flex justify-center">
+        <div className="mr-auto">
+          <h1 className="text-4xl font-bold">Shortcuts</h1>
+        </div>
+        <div className="ml-auto">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center justify-center space-x-2">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 float-left mr-auto" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
+              </svg>
+              <a href="/createlink">New Shortcut</a>
+            </button>
+        </div>
+      </div>
+      <div className="min-h-screen stock-container">{isLoading ? <LoadingSpinner /> : links}</div>
       </>
     );
   } else {
